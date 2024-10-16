3cc0d4eb82063532ac2688247f65702af0b09995
Maximilian Vogt <vogtmh@gmail.com>
2024-10-17 00:32:25 +0700

    initial commit for v1.0.0

---
+++ LightSwitch/js/main.js
@@ -0,0 +1,62 @@
+﻿var appstring;
+
+(function () {
+    let lamp = null;
+
+    // Asynchronously get access to the flashlight (Lamp)
+    Windows.Devices.Lights.Lamp.getDefaultAsync().then(function (defaultLamp) {
+        if (defaultLamp) {
+            try {
+                lamp = defaultLamp;
+                lamp.isEnabled = true;
+            }
+            catch (e) {
+                console.log(e.message);
+            }
+        }
+    });
+
+    var versionlabel = document.getElementById("versionlabel");
+    try {
+        appVersion = Windows.ApplicationModel.Package.current.id.version;
+        appstring = `${appVersion.major}.${appVersion.minor}.${appVersion.build}`;
+    }
+    catch (e) {
+        appstring = '';
+    }
+    versionlabel.innerHTML = "<a href='https://github.com/vogtmh/lightswitch'>LightSwitch</a> v" + appstring;
+
+    // This code was meant to prevent the app getting closed on suspend, but didn't work
+    // I'm leaving it here, so someone might be able to solve it.
+
+    /*
+    function onSuspending(event) {
+        let deferral = event.suspendingOperation.getDeferral();
+
+        // Turn off the flashlight if it's on
+        if (lamp && lamp.isEnabled) {
+            lamp.isEnabled = false;
+        }
+
+        // Complete the deferral
+        deferral.complete();
+    }
+
+    function onResuming(event) {
+        if (defaultLamp) {
+            try {
+                lamp = defaultLamp;
+                lamp.isEnabled = true;
+            }
+            catch (e) {
+                console.log(e.message);
+            }
+        }
+    }
+
+    // Register for the suspending/resuming events
+    Windows.UI.WebUI.WebUIApplication.addEventListener("suspending", onSuspending);
+    Windows.UI.WebUI.WebUIApplication.addEventListener("resuming", onResuming);
+    */
+
+})();
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# lightswitch
+# LightSwitch
 A simple app to turn on the light on Windows 10 smartphones
+
+* turns on the flashlight when launched
+* turns the lights off when suspended and will close automatically
+* lockscreen will have the same effect
+* please allow 3-5s for the app to close before reopening it
+
+What's the benefit of this app?
+* CMD Injector allows you to reassign the search button
+* you can assign it to this app
+* then unlock your phone, hit the search button and you got a light
+* use the back button, the home button or simply lock the phone to turn it off again
